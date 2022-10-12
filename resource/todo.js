c5ccf38e7c801d20d3660d3369b40fb59bf1c10e
kreetika20 <019bim020@sxc.edu.np>
2022-10-12 14:31:29 +0545

    project work

---
+++ resource/todo.js
@@ -0,0 +1,22 @@
+const todos = [{
+        "_id": "001",
+        "title": "Complete bootcamp project",
+        "description": "Talk to team,make team repository,assign task",
+
+    },
+    {
+        "_id": "002",
+        "title": "Git assignments",
+        "description": "Upload in classroom",
+    }, {
+        "_id": "003",
+        "title": "Go to market",
+        "description": "Buy groceries",
+    }, {
+        "_id": "004",
+        "title": "Cooking",
+        "description": "Make dinner for family",
+    }
+]
+
+module.exports = todos;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var todos = require('../resource/todo');
+// console.log(todos);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Kritika' });
+    res.render('home', { todosList: todos });
 });
 
 router.get('/home', function(req, res, next) {
     res.render('home');
 });
 
+router.get('/add-to-do', function(req, res, next) {
+    res.render('addTodo', { title: 'Add To do' });
+})
+
+router.post('/save-to-do', function(req, res, next) {
+    todos.push({...req.body, _id: `00${todos.length}` });
+    res.redirect('/');
+})
+
+router.get('/delete-to-do/:index', function(req, res, next) {
+    todos.splice(req.params.index, 1);
+    res.redirect('/');
+})
+
+router.get('/open-update-form/:id', function(req, res, next) {
+    const todotodo = todos.find(todo => todo._id === req.params.id);
+    res.render('editToDo', { todo: todotodo });
+})
+router.post('/update-to-do/:id', function(req, res, next) {
+    const index = todos.findIndex(todo => todo._id === req.params.id);
+    todos.splice(index, 1, {...req.body, _id: req.params.id });
+    res.redirect('/');
+})
 
 module.exports = router;
